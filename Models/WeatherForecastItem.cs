0cfba73ab8630c8d8fc0ed65b701eacc79d1fd48
Pedro Menezes <pedropltm@gmail.com>
2022-01-31 08:41:07 -0300

    Fixing the class name.

--- Models/WeatherForecastItem.cs
+++ Models/WeatherForecastItem.cs
@@ -1,6 +1,6 @@
 namespace WeatherForecastAPI.Models
 {
-    public class WeatherForecast
+    public class WeatherForecastItem
     {
         public long Id { get; set; }
         public string? Name { get; set; }
